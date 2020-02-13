ce7909e3291d08255621a7761b41d9d1e97b637b
ybcj <ybcj_lv@163.com>
2020-02-13 19:29:17 +0800

    条件和循环

---
+++ src/ch5/condtion/condtion_test.go
@@ -0,0 +1,35 @@
+package condtion_test
+
+import "testing"
+
+func TestIfMultiSec(t *testing.T) {
+	if a := 1 == 1; a {
+		t.Log("1 == 1")
+	}
+}
+
+func TestSwitchMultiCase(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		switch i {
+		case 0, 2:
+			t.Log("Even")
+		case 1, 3:
+			t.Log("Odd")
+		default:
+			t.Log("it is not 0-5")
+		}
+	}
+}
+
+func TestSwitchCaseCondition(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		switch {
+		case i%2 == 0:
+			t.Log("Odd")
+		case i%2 == 1:
+			t.Log("Even")
+		default:
+			t.Log("nukonw")
+		}
+	}
+}
