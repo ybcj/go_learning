3fd45413559036fe3f8b0bcea0aa39a69340b759
ybcj <ybcj_lv@163.com>
2020-02-21 23:40:16 +0800

    错误处理

---
+++ src/ch14/error/error_test.go
@@ -0,0 +1,42 @@
+package err_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+var LessThanTwoError = errors.New("n should be not less than 2")
+var LargerThanHandredError = errors.New("n should be not larger than 100")
+
+func GetFibonacci(n int) ([]int, error) {
+	// if n < 0 || n > 100 {
+	// 	return nil, errors.New("n should bi in [2,100]")
+	// }
+
+	if n < 2 {
+		return nil, LessThanTwoError
+	}
+	if n > 100 {
+		return nil, LargerThanHandredError
+	}
+
+	fibList := []int{1, 1}
+	for i := 2; i < n; i++ {
+		fibList = append(fibList, fibList[i-2]+fibList[i-1])
+	}
+	return fibList, nil
+}
+
+func TestGetFibonacci(t *testing.T) {
+	if v, err := GetFibonacci(-10); err != nil {
+		if err == LessThanTwoError {
+			fmt.Println("It is less.")
+		}
+		t.Error(err)
+	} else {
+		t.Log(v)
+	}
+}
+
+// 及早失败，避免嵌套
