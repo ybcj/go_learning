061577b25d3f37ecaf52e79193f66972ab2b45ae
ybcj <ybcj_lv@163.com>
2020-02-23 01:18:17 +0800

    CSP

---
+++ src/ch18/csp/async_service_test.go
@@ -0,0 +1,42 @@
+package asyncservice
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("working on somethind else")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done.")
+}
+
+func TestService(t *testing.T) {
+	fmt.Println(service())
+	otherTask()
+}
+
+func AsyncServer() chan string {
+	retCh := make(chan string)
+	// retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+		fmt.Println("service exited")
+	}()
+	return retCh
+}
+
+func TestAsyncServer(t *testing.T) {
+	retCh := AsyncServer()
+	otherTask()
+	fmt.Println(<-retCh)
+	time.Sleep(time.Second * 1)
+}
